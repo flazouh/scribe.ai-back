fdc5e2d7adb317307e6595bb0712888e0807e2b7
flazoo <alexandredepape@gmail.com>
2024-10-28 23:20:53 +0100

    ✨ (package.json, main.ts, transcription.gateway.ts, transcription.service.ts, transcription.types.ts): enhance transcription service and WebSocket communication 🔧 (package.json): add @types/fluent-ffmpeg as a development dependency
    The transcription service has been improved to handle audio processing more effectively, including better error handling and logging. The WebSocket communication has been enhanced to provide clearer event names and payload structures. Additionally, the package.json file now includes type definitions for fluent-ffmpeg to improve type safety during development. These changes aim to enhance the overall robustness and maintainability of the transcription feature.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, PartialGraphHost } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as fs from 'fs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
@@ -8,4 +8,8 @@ async function bootstrap() {
   
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error("Bootstrap process failed:", err);
+  fs.writeFileSync("graph.json", PartialGraphHost.toString() ?? "");
+  process.exit(1);
+});
--- src/transcription/transcription.gateway.ts
+++ src/transcription/transcription.gateway.ts
@@ -1,45 +1,61 @@
-import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect, MessageBody, ConnectedSocket } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { TranscriptionService } from './transcription.service';
 import {
-  AudioPayload,
+  TranscripEventCallbacks,
   TranscriptionEvents,
+  TranscriptionPayload,
 } from './types/transcription.types';
+import { Injectable, Logger } from '@nestjs/common';
+import { sendWSEvent } from './utils';
 
-enum WebsocketRequestEvents {
-  REQUEST_TRANSCRIPTION = 'REQUEST_TRANSCRIPTION',
+
+enum TranscriptionWebsocketMessages {
+  REQUEST_TRANSCRIPTION = 'request_transcription',
 }
 
+enum TranscriptionWebsocketNamespaces {
+  TRANSCRIPTION = 'transcription',
+}
+interface AuthenticatedWebSocketMessageBody<T> {
+  payload: T;
+}
+interface TestPayload {
+  message: string;
+}
+@Injectable()
 @WebSocketGateway({
+  namespace: TranscriptionWebsocketNamespaces.TRANSCRIPTION,
   cors: {
     origin: '*', // Configure according to your frontend origin
   },
+  maxHttpBufferSize: 1e7
 })
-export class TranscriptionGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  @WebSocketServer()
-  server: Server;
+export class TranscriptionGateway {
 
+  private readonly logger = new Logger(TranscriptionGateway.name);
   constructor(private readonly transcriptionService: TranscriptionService) { }
 
-  handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
+  @SubscribeMessage("test")
+  async test(@ConnectedSocket() client: Socket, @MessageBody() { payload }: AuthenticatedWebSocketMessageBody<TestPayload>) {
+    this.logger.log(`Received test message from client: ${client.id}: ${payload.message}`);
+    client.emit("test", "test");
   }
-
-  handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
-  }
-
-  @SubscribeMessage(WebsocketRequestEvents.REQUEST_TRANSCRIPTION)
-  async handleTranscription(client: Socket, payload: AudioPayload) {
+  @SubscribeMessage("request_transcription")
+  async handleTranscription(@MessageBody() { payload }: AuthenticatedWebSocketMessageBody<TranscriptionPayload>,
+    @ConnectedSocket() client: Socket) {
+    this.logger.log(`Received transcription request from client: ${client.id}`);
     try {
-
+      const buffer = Buffer.from(payload.buffer);
       await this.transcriptionService.processAudio(
-        { audioData: payload.audioData, client });
+        { audioData: buffer, client });
 
     } catch (error) {
-      client.emit(TranscriptionEvents.PROCESS_FINISHED, {
-        error: error.message
+      sendWSEvent<TranscriptionEvents, TranscripEventCallbacks>(client, TranscriptionEvents.PROCESS_FINISHED, {
+        error: error.message,
+        message: "There was an error processing your request"
       });
+      this.logger.error(`Error processing transcription: ${error.message}`);
     }
   }
 }
